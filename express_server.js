03c303bc1691e40ca17d856a28d8ed71bd6abedc
vanlgo <goellevan@gmail.com>
2021-11-27 23:20:53 +0000

    added generate random string function

--- express_server.js
+++ express_server.js
@@ -2,12 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs")
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  const char = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+  let result = "";
+  while (result.length < 6) {
+    result += char.charAt(Math.floor(Math.random() * char.length));
+  }
+  return result;
+  }
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -24,6 +36,15 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
